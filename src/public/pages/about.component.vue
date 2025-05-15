501de14d9e503e355bdead12fda980693dde8462
MatFragg <ethan.aliaga@gmail.com>
2025-05-15 18:50:37 -0500

    root-commit

---
+++ src/public/pages/about.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "about",
+  title: "About Us",
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>About Us</h1>
+    <p>ElectroLink is a platform that aims for connecting , part of ACME Corporation.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
